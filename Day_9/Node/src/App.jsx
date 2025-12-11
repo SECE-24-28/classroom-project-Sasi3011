7016178e7704a5abc223798ba085319665b619d4
Sasi <sasikiran.tt3011@gmail.com>
2025-12-11 09:39:51 +0530

    Fetching data from db.json

--- Day_9/Node/src/App.jsx
+++ Day_9/Node/src/App.jsx
@@ -1,33 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
+import api from "./api/Courses"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [CList, setCList] = useState([])
+
+  useEffect(() => {
+    const fetData = async () => {
+      try {
+        const response = await api.get("/courses")
+        setCList(response.data)
+        // console.log("Data: ", response.data)
+      } catch (err) {
+        console.log("Error: ", err)
+      }
+    }
+    fetData()
+  }, [])
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {
+        CList.map((course) => (
+          <p key={course.id}>
+            {course.id} - {course.name} - {course.level}
+          </p>
+        ))
+      }
     </>
   )
 }
