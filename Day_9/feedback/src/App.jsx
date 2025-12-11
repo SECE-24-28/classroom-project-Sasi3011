1375a780c976541999cd1f4767e37e7c96a935ec
Sasi <sasikiran.tt3011@gmail.com>
2025-12-11 11:48:02 +0530

    Added post

--- Day_9/feedback/src/App.jsx
+++ Day_9/feedback/src/App.jsx
@@ -1,7 +1,30 @@
 import React from 'react'
 import './App.css'
-
+import { useState } from 'react'
+import { useEffect } from 'react'
+import api from './api/Feedback'
 function App() {
+  const[post,setPost]=useState([])
+
+  useEffect(()=>{
+    const fetData=async()=>{
+      const response=await api.get("/feedback")
+      console.log(response.data)
+      setPost(response.data)  
+    }
+    fetData();
+  },[])
+  return (
+    <>
+      {
+        post.map((post)=>(
+          <p key={post.id}>
+            {post.id} - {post.title} - {post.timedate} - {post.body}
+          </p>
+        ))
+      }
+    </>
+  )
 }
 
 export default App
